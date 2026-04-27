9e46740c5b776697298a9177a37fde2b39e5f6db
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-04-27 14:25:30 +0400

    Update hook_impl.h

--- src/hook/hook_impl.h
+++ src/hook/hook_impl.h
@@ -9,6 +9,7 @@ extern "C" {
 
 #include <stdio.h>
 #include <android/dlext.h>
+#include <sys/system_properties.h>
 
 void init_hook_param(const void *param);
 
@@ -23,6 +24,10 @@ FILE *hook_fopen(const char *filename, const char *mode);
 int hook_gsl_memory_alloc_pure_64(uint64_t size, uint32_t flags, void *memDesc);
 
 int hook_gsl_memory_free_pure(void *memDesc);
+
+int hook___system_property_get(const char *name, char *value);
+
+void hook___system_property_read_callback(const prop_info *pi, void (*callback)(void *, const char *, const char *, uint32_t), void *cookie);
 
 #ifdef __cplusplus
 }
